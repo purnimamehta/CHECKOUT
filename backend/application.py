01a1edb06d8334148f9cbc97df13e0dc72061f29
vatdaell <ansafahmad70@gmail.com>
2015-11-28 02:04:00 -0500

    Added most of the routes

--- backend/application.py
+++ backend/application.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template,g,request
 app = Flask(__name__)
 
-@app.route('/AnsafAhmad')
-def hello_world():
-    return 'True'
+@app.route('/store', methods = ['GET', 'POST'])
+def store():
+	 store = request.args.get('store')
+	 return str(store)
 
-@app.route('/Fiza')
-def Fiza():
-    return 'fiza'
+@app.route('/upc', methods = ['GET', 'POST'])
+def upc():
+	 name = request.args.get('upc')
+	 return str(name)
+
+@app.route('/user', methods = ['GET', 'POST'])
+def store():
+	 username = request.args.get('username')
+	 password = request.args.get('password')
+	 return str(name) + " " + str(password)
 
 if __name__ == '__main__':
     app.run()
