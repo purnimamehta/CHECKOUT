290e29874a9552c01fb6cefe0616da5b2ce1821d
Sagar <sagarthomas27@hotmail.com>
2015-11-28 13:21:28 -0500

    Commented BarCodeActivity

--- frontend/Checkout/app/src/main/java/com/app/checkout/BarCodeActivity.java
+++ frontend/Checkout/app/src/main/java/com/app/checkout/BarCodeActivity.java
@@ -11,34 +11,48 @@ import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-
+/* ********************************************************************
+    @Author: Sagar                 @Date: 2015 - 11 - 28
+    *Class to handle all Barcode Scanning
+ * ********************************************************************/
 
 public class BarCodeActivity extends ActionBarActivity {
+    private String barcode; // Variable to hold inputted barcode scan
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar_code);
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(AnyOrientationCaptureActivity.class);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+        integrator.setPrompt("Scan something"); // Optional- take out or change if needed
+        integrator.setOrientationLocked(false); // Scanning can be done in any orientation
+        integrator.setBeepEnabled(false);
+        integrator.initiateScan();
     }
 
     public void scanBarcode(View view) {
         new IntentIntegrator(this).initiateScan();
     }
-
+    // TODO: Get rid of method after success in connecting to ShoppingCartActivity
     public void scanBarcodeCustomLayout(View view) {
-        IntentIntegrator integrator = new IntentIntegrator(this);
-        integrator.setCaptureActivity(AnyOrientationCaptureActivity.class);
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
-        integrator.setPrompt("Scan something");
-        integrator.setOrientationLocked(false);
-        integrator.setBeepEnabled(false);
-        integrator.initiateScan();
+
     }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        barcode = result.getContents(); // Store scanned value here
+        // Code for sending barcode variable to ShoppingCartActivity
+        Intent intent = new Intent(getApplicationContext(), ShoppingCartActivity.class); // create new Intent for sending to ShoppingCartActivity
+        intent.putExtra("barcode", barcode); // Add barcode variable to 'extra' in the Intent
+        startActivity(intent);
+
+        //TODO: Delete commented code after ShoppingCartActivity works
+        /*
         if(result != null) {
             if(result.getContents() == null) {
                 Log.d("MainActivity", "Cancelled scan");
@@ -52,6 +66,15 @@ public class BarCodeActivity extends ActionBarActivity {
             // This is important, otherwise the result will not be passed to the fragment
             super.onActivityResult(requestCode, resultCode, data);
         }
+        */
+
+        // TODO: Add following code block to ShoppingCartActivity after set up
+        /*
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            barcode = extras.getString("barcode");
+        }
+         */
     }
 
     @Override
