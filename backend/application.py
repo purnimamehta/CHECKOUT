062069e960c59ae2adbc965c091dc7be7d124ecc
vatdaell <ansafahmad70@gmail.com>
2015-11-28 11:24:41 -0500

    Adding orchestrate io db

--- backend/application.py
+++ backend/application.py
@@ -1,4 +1,8 @@
 from flask import Flask,request
+import porc
+
+API_KEY = '16509990-f7fc-48dc-b128-716c99b65a24'
+
 app = Flask(__name__)
 
 @app.route('/store', methods = ['GET', 'POST'])
@@ -15,10 +19,18 @@ def upc():
 def user():
 	 username = request.args.get('username')
 	 password = request.args.get('password')
-	 return str(name) + " " + str(password)
+	 result = 'False'
+
+	 users_check = client.get('users',username)
+
+	 if(len(users_check)>0 and users_check['password'] == password):
+	 	result = 'True'
+
+
+	 return result
 
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host = '0.0.0.0')
