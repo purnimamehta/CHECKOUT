558c7cdc0c9b7eff4b76f4c3474fad569e6c6291
vatdaell <ansafahmad70@gmail.com>
2015-12-26 04:15:16 -0500

    Added django

---
+++ backend/ezcheckout/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ansaf ahmad\desktop\checkout\backend\ezcheckout\scripts\python2.7.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
