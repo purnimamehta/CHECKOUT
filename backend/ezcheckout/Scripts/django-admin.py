aa656fe9323a27224ca4f0dd0c9dc2c6850e833b
vatdaell <ansafahmad70@gmail.com>
2016-01-04 00:07:07 -0500

    Removed Backend for now, working on a new one

--- backend/ezcheckout/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\ansaf ahmad\desktop\checkout\backend\ezcheckout\scripts\python2.7.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
